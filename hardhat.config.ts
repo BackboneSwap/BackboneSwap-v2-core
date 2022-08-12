852d1f033ec6a03234bcbb0e0aa22bd6bb9bdfe2
kofkuiper <kofkuiper@gmail.com>
2022-08-12 10:20:09 +0700

    add ivarex mainnet and bitkub testnet config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,8 @@
 import * as dotenv from 'dotenv'
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-const {
-  getEtherscanEndpoints,
-} = require('@nomiclabs/hardhat-etherscan/dist/src/network/prober')
 
 dotenv.config()
-
-const chainConfig: any = {
-  ivarextestnet: {
-    chainId: 16888,
-    urls: {
-      apiURL: 'https://testnet.ivarscan.com/api',
-      browserURL: 'https://testnet.ivarscan.com/'
-    }
-  }
-}
 
 const config: HardhatUserConfig = {
   solidity: "0.5.16",
@@ -24,11 +11,23 @@ const config: HardhatUserConfig = {
       url: 'https://testnet.ivarex.com',
       chainId: 16888,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    }
+    },
+    ivarex: {
+      url: 'https://mainnet.ivarex.com',
+      chainId: 88888,
+      accounts: process.env.PRIVATE_KEY_DEPLOY_IVAREX ? [process.env.PRIVATE_KEY_DEPLOY_IVAREX] : []
+    },
+    bitkubtestnet: {
+      url: 'https://rpc-testnet.bitkubchain.io',
+      chainId: 25925,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
   },
   etherscan: {
     apiKey: {
-      ivarextestnet: ':D'
+      ivarex: 'ivarex',
+      ivarextestnet: ':D',
+      bitkubtestnet: 'bitkubtestnet'
     },
     customChains: [
       {
@@ -38,7 +37,23 @@ const config: HardhatUserConfig = {
           apiURL: 'https://testnet.ivarscan.com/api',
           browserURL: 'https://testnet.ivarscan.com/'
         }
-      }
+      },
+      {
+        network: 'ivarex',
+        chainId: 88888,
+        urls: {
+          apiURL: 'https://ivarscan.com/api',
+          browserURL: 'https://ivarscan.com/'
+        }
+      },
+      {
+        network: 'bitkubtestnet',
+        chainId: 25925,
+        urls: {
+          apiURL: 'https://testnet.bkcscan.com/api',
+          browserURL: 'https://testnet.bkcscan.com/'
+        }
+      },
     ]
   }
 };
